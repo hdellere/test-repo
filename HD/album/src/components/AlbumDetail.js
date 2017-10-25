42711fd6b750bddc122e7a5f9ac282df19099aae
Hunter Dellere <hdellere@gmail.com>
2017-10-24 19:56:37 -0500

    Lecture 58

--- HD/album/src/components/AlbumDetail.js
+++ HD/album/src/components/AlbumDetail.js
@@ -3,13 +3,71 @@
 import React from 'react';
 import {
   Text,
+  View,
+  Image,
+  Linking
 } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => (
-  <Card>
-    <Text>{props.album.title}</Text>
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image, image, url } = album;
+
+  return (
+    <Card>
+      <CardSection>
+        <View style={styles.imageContainerStyle}>
+          <Image
+            source={{ uri: thumbnail_image }}
+            style={styles.imageStyle}
+          />
+        </View>
+        <View style={styles.headerStyle}>
+          <Text style={styles.headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image
+          style={styles.mainImageStyle}
+          source={{ uri: image }}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)} >
+          Oh Hello, I Am Button
+        </Button>
+      </CardSection>
   </Card>
-);
+  );
+};
+
+const styles = {
+  headerStyle: {
+    justifyContent: 'space-around',
+    flexDirection: 'column',
+  },
+  imageStyle: {
+    height: 50,
+    width: 50
+  },
+  imageContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  mainImageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
 
 export default AlbumDetail;
---
+++ HD/auth/src/App.js
@@ -0,0 +1,55 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Header,
+  Card,
+  CardSection,
+  Button
+} from './components/common';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View>
+        <Header headerText='Authentication App' />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2abfff',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
