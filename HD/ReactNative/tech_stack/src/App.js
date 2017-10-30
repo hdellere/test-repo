a028f1732382afe6cc684b2f779872a03f8e969f
Hunter Dellere <hdellere@gmail.com>
2017-10-29 19:26:50 -0500

    Section 12 complete, basics of redux

--- HD/ReactNative/tech_stack/src/App.js
+++ HD/ReactNative/tech_stack/src/App.js
@@ -6,12 +6,14 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
       <View>
         <Header headerText="Tech Stacker" />
+        <LibraryList />
       </View>
     </Provider>
   );
